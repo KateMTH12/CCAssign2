088db11654dcf9806a75a0294f5b6945bb953dc6
KateMTH12 <KateMTH12@users.noreply.github.com>
2017-02-06 03:40:58 -0500

    Add files via upload

---
+++ assignment2.ino
@@ -0,0 +1,48 @@
+int greenLED = 9;
+int redLED = 10;
+int sensor = A0;
+int timesUp = 10000;
+long starting = 0;
+long countDown = 0;
+boolean LEDOn = false;
+boolean blinking = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+}
+
+void loop() {
+  int reading = analogRead(sensor);
+  Serial.println(reading);
+  delay(10);
+  if (reading <= 600 || blinking == true) {
+    digitalWrite(greenLED, LOW);
+    LEDOn = false;
+  }
+  if (blinking == false || reading <= 600) {
+    digitalWrite(redLED, LOW);
+    LEDOn = false;
+    blinking = false;
+  }
+  if (reading > 600 && blinking == false) {
+    digitalWrite(greenLED, HIGH);
+    LEDOn = true;
+    blinking = false;
+  }
+  if (LEDOn == true && millis() - starting > timesUp) {
+    digitalWrite(greenLED, LOW);
+    delay(100);
+    digitalWrite(greenLED, HIGH);
+    delay(100);
+    starting = millis();
+    blinking = true;
+    LEDOn = true;
+  }
+  if ( blinking == true && micros() - countDown > 2*timesUp) {
+    digitalWrite(redLED, HIGH);
+    countDown = micros();
+    LEDOn = false;
+  }
+}
